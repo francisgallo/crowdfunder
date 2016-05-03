cda2cba1075afccdf057ce05510e67adc24c138f
Kimberly Oleiro <koleiro@gmail.com>
2016-05-03 19:34:27 -0400

    Added column to Project Controller and allowed them in project_params

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503204912) do
+ActiveRecord::Schema.define(version: 20160503233252) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160503204912) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "rewards", force: :cascade do |t|
