139608d6ad45980cb4734abb8c3dec6a08c997a1
CALLUM BURNS <CBU787@GMAIL.COM>
2016-05-03 20:10:25 -0400

    Created 2 users to seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(name: 'John Smith', email: 'john@gmail.com', password: 'Blue123', password_confirmation: 'Blue123')
+User.create(name: 'Kate Jones', email: 'kate@gmail.com', password: 'Blue123', password_confirmation: 'Blue123')
