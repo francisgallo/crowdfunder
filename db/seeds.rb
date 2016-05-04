d7c56f81e92138dd963e46efeaa182f216890ad5
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-03 20:12:16 -0400

    Seeded 9 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.destroy_all
+#1
+Category.create!(
+  :name => 'art'
+)
+#2
+Category.create!(
+  :name => 'crafts'
+)
+#3
+Category.create!(
+  :name => 'design'
+)
+#4
+Category.create!(
+  :name => 'fashion'
+)
+#5
+Category.create!(
+  :name => 'food'
+)
+#6
+Category.create!(
+  :name => 'games'
+)
+#7
+Category.create!(
+  :name => 'music'
+)
+#8
+Category.create!(
+  :name => 'photography'
+)
+#9
+Category.create!(
+  :name => 'technology'
+)
