9e9fe7c0a594b574bdaaa6c33f1d349fa725b875
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-03 23:38:03 -0400

    Fixed the seeds and created an index and show for categories views

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,36 +9,36 @@
 User.destroy_all
 Category.destroy_all
 Project.destroy_all
-
-Project.create(
-  title: "Corduroy",
-  description: "Door-to-door laundry and dry cleaning service. Find a dry cleaner in your area and we'll take care of all the rest. Just schedule a pick-up",
-  goal: 20000,
-  start_date: 2016-02-06T00:00:00+00:00,
-  end_date: 2016-06-06T00:00:00+00:00,
-  location: "Toronto",
-  user_id: 1,
-  category_id: 9,
-  image: "http://previews.123rf.com/images/pitris/pitris1302/pitris130200002/17816335-Illustration-of-clothes-drying-outdoor--Stock-Vector-clothes-laundry-hanging.jpg"
+#1
+Project.create!(
+  :title => "Corduroy",
+  :description => "Door-to-door laundry and dry cleaning service. Find a dry cleaner in your area and we'll take care of all the rest. Just schedule a pick-up",
+  :goal => 20000,
+  :start_date => DateTime.parse("06/02/2016 00:00"),
+  :end_date => DateTime.parse("06/06/2016 00:00"),
+  :location => "Toronto",
+  :user_id => 1,
+  :category_id => 9,
+  :image => "http://previews.123rf.com/images/pitris/pitris1302/pitris130200002/17816335-Illustration-of-clothes-drying-outdoor--Stock-Vector-clothes-laundry-hanging.jpg"
 )
-
-Project.create(
-  title: "Sensory Dessert",
-  description: "Full satisfaction without the calories. Allow our product to create the percieved satisfaction of eating a dessert through currated sense combinations designed to make you feel satisfied.",
-  goal: 30000,
-  start_date: 2016-03-01T00:00:00+00:00,
-  end_date: 2016-07-01T00:00:00+00:00,
-  location: "Toronto",
-  user_id: 2,
-  category_id: 9,
-  image: "https://s-media-cache-ak0.pinimg.com/736x/1a/2d/ce/1a2dce5ba34349558f00a792e0dcce99.jpg"
-  ) 
+#2
+Project.create!(
+  :title => "Sensory Dessert",
+  :description => "Full satisfaction without the calories. Allow our product to create the percieved satisfaction of eating a dessert through currated sense combinations designed to make you feel satisfied.",
+  :goal => 30000,
+  :start_date => DateTime.parse("03/01/2016 00:00"),
+  :end_date => DateTime.parse("07/01/2016 00:00"),
+  :location => "Toronto",
+  :user_id => 2,
+  :category_id => 9,
+  :image => "https://s-media-cache-ak0.pinimg.com/736x/1a/2d/ce/1a2dce5ba34349558f00a792e0dcce99.jpg"
+  )
 
 
 #1
-User.create(name: 'John Smith', email: 'john@gmail.com', password: 'Blue123', password_confirmation: 'Blue123')
+User.create!(name: 'John Smith', email: 'john@gmail.com', password_digest: 'Blue123')
 #2
-User.create(name: 'Kate Jones', email: 'kate@gmail.com', password: 'Blue123', password_confirmation: 'Blue123')
+User.create!(name: 'Kate Jones', email: 'kate@gmail.com', password_digest: 'Blue123')
 
 #1
 Category.create!(
@@ -75,5 +75,4 @@ Category.create!(
 #9
 Category.create!(
   :name => 'technology'
->>>>>>> 015982c38982b05158894c847aaf8b356054a741
 )
