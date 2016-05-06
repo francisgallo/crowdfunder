5463612e56e7cc271d640621a9bb75425cd2f42e
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-05-05 21:01:15 -0400

    created total pledges

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,10 @@ class Project < ActiveRecord::Base
   belongs_to :category
 
   accepts_nested_attributes_for :rewards,  reject_if:  :all_blank
+
+  def total_pledges
+    return self.pledges.sum(:amount)
+  end
+
+
 end
